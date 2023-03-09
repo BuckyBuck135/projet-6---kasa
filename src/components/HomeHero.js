30ab545d96d8a2c90df73225937afebc02bde99e
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-03-09 16:24:02 +0100

    changed hero html to prepare for component

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -4,6 +4,7 @@ import "../css/HomeHero.css"
 export default function HomeHero() {
     return (
         <section className="hero">
+            <img src="images/eric-muhr-P_XxsdVgtpQ-unsplash.jpg" className="hero--image" alt="Paysage en plan large" />
             <h1 className="hero--title">Chez vous, partout et ailleurs</h1>
         </section>
     )
