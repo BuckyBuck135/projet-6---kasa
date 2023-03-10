1cbd87d0c7fef0403577b5c30ec880de58c6db51
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-03-10 11:09:31 +0100

    Preparing App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.js"
-import Hero from "./components/Hero/Hero.js"
 import Home from "./pages/home/Home.js"
 import NotFound from "./pages/not-found/NotFound.js"
 import APropos from "./pages/a-propos/APropos.js"
 import FicheLogement from "./pages/fiche-logement/FicheLogement.js"
-
 import Footer from "./components/Footer/Footer.js"
 
-
+// renders Navbar, Footer, and the 4 pages withing the container
 export default function App() {
   return (
     <div className="app">
       <Navbar />
       <div className="container">
-      <APropos />
-      <Home />
+        <Home />
+        <APropos />
+        <FicheLogement />
+        <NotFound />
       </div>
       <Footer />
     </div>
@@ -24,7 +24,6 @@ export default function App() {
 }
 
 // removed from App() until I know how to organize the different pages
-// <Hero />
 // <Home />
 // <NotFound />
 // <APropos />
