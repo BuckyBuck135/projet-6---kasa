11b6ad100ab7477ef559cdbff849ab7dcd989c43
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-03-21 16:44:37 +0100

    Refactored slider + transitions

--- src/components/Carrousel/Carrousel.js
+++ src/components/Carrousel/Carrousel.js
@@ -5,25 +5,33 @@ import previousArrow from "../../images/previous-arrow.svg"
 
 export default function Carrousel(props) {
     const maxIndex = props.listing.pictures.length - 1
-    const [index, setIndex] = React.useState(0)
+    const [currentIndex, SetCurrentIndex] = React.useState(0)
 
-    const currentImage = props.listing.pictures[index]
+    // const currentImage = props.listing.pictures[currentIndex]
+    const allImages = props.listing.pictures.map((image, index) => {
+        return (
+            <div className={currentIndex === index ? "carrousel--slide carrousel--active" : "carrousel--slide"}>
+                {index === currentIndex && <img src={image} className="carrousel--image" alt="Carrousel d'images du logement." />}
+            </div>
+        )
+    })
     
 
     function prevImage() {
-        setIndex(prevIndex => prevIndex === 0 ? prevIndex = maxIndex : prevIndex = prevIndex - 1)
+        SetCurrentIndex(prevIndex => prevIndex === 0 ? prevIndex = maxIndex : prevIndex = prevIndex - 1)
     }
 
     function nextImage() {
-        setIndex(prevIndex => prevIndex === maxIndex ? prevIndex = 0 : prevIndex = prevIndex + 1)
+        SetCurrentIndex(prevIndex => prevIndex === maxIndex ? prevIndex = 0 : prevIndex = prevIndex + 1)
     }
 
     return (
         <figure className="carrousel--container">
+            {allImages}
             <img src={previousArrow} onClick={prevImage} className="carrousel--arrow previous" alt="Icône de défilement en avant du carrousel."/> 
-            <img src={currentImage} className="carrousel--image animate" alt="Carrousel d'images du logement." />
+            
             <img src={nextArrow} onClick={nextImage} className="carrousel--arrow next" alt="Icône de défilement en arrière du carrousel."/> 
-            <figcaption className="carrousel--caption">{index+1}/{maxIndex+1}</figcaption>
+            <figcaption className="carrousel--caption">{currentIndex+1}/{maxIndex+1}</figcaption>
         </figure>
     )
 }
