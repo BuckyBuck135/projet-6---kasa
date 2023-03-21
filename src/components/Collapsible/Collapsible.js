0a0f25621c068c7e04a99f075a1bd4e9838c69e7
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-03-21 15:10:38 +0100

    Working collapsibles - local state management

--- src/components/Collapsible/Collapsible.js
+++ src/components/Collapsible/Collapsible.js
@@ -1,27 +1,35 @@
 import React from "react";
 import "./Collapsible.css"
-import downArrow from "../../images/down-arrow.png"
+import downArrow from "../../images/down-arrow.svg"
+import upArrow from "../../images/up-arrow.svg"
 
 // implement collapsible arrow function
 
 export default function Collapsible(props) {
+
+    const [isShown, setIsShown] = React.useState(false)
+
+    function toggle() {
+        setIsShown(prev => !prev)
+    }
+
+    // managing content of the collapsible depending on type: paragraph or list
     let content 
-    if(props.type == "paragraph") {
+    if(props.type === "paragraph") {
         content = (<p className="collapsible--description">{props.description}</p>)
-    } else if (props.type == "list"){
+
+    } else if (props.type === "list"){
         const amenities = props.equipments.map((item, index) => <li key={index} className="collapsible--li">{item}</li>)
-        content = (<ul>{amenities}</ul>)
+        content = (<ul className="collapsible--description">{amenities}</ul>)
     } 
     
     return (
         <article key={props.id} className="collapsible">
-            <div className="collapsible--title-container">
+            <div onClick={toggle} className="collapsible--title-container">
                 <h3 className="collapsible--title">{props.title}</h3>
-                <img src={downArrow} className="collapsible--icon" alt="icône flèche vers le bas."/>
+                <img src={isShown ? upArrow : downArrow} className="collapsible--icon" alt="icône d'ouverture ou fermeture."/>
             </div>
-            <div className="collapsible--text-container">
-                {content}
-            </div>
+            {isShown && <div className="collapsible--text-container animate">{content}</div>}
         </article>
     )
     } 
