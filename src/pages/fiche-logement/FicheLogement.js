607183a01aa3e8bebf24a7fa330c3a2e88ecb851
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-03-22 16:35:03 +0100

    handled route error

--- src/pages/fiche-logement/FicheLogement.js
+++ src/pages/fiche-logement/FicheLogement.js
@@ -1,13 +1,12 @@
 import React from "react"
-import { useParams } from "react-router-dom";
-import "./FicheLogement.css"
+import {useParams} from "react-router-dom";
 import Carrousel from "../../components/Carrousel/Carrousel";
 import Tag from "../../components/Tag/Tag"
 import Collapsible from "../../components/Collapsible/Collapsible"
 import StarRating from "../../components/StarRating/StarRating"
 import Host from "../../components/Host/Host"
-
-
+import NotFound from "../not-found/NotFound";
+import "./FicheLogement.css"
 
 const data = require("../../data/logements.json");
 
@@ -15,50 +14,56 @@ export default function FicheLogement() {
     const params = useParams()
     const listing = data.find(item => item.id === params.id)
 
-    return (
-        <section className="main">
-            
-            <Carrousel 
-                listing={listing}
-            />
-
-            <div className="listing--content">
-                <div className="listing--content-wrapper">
-                    <div className="listing--wrapper-left">
-                        <div className="listing--heading">
-                            <h1>{listing.title}</h1>
-                            <h2>{listing.location}</h2>
+    if (!listing) {
+        return <NotFound />
+    } else {
+        return ( 
+            <section className="main">
+                
+                <Carrousel 
+                    listing={listing}
+                />
+    
+                <div className="listing--content">
+                    <div className="listing--content-wrapper">
+                        <div className="listing--wrapper-left">
+                            <div className="listing--heading">
+                                <h1>{listing.title}</h1>
+                                <h2>{listing.location}</h2>
+                            </div>
+                            <Tag 
+                                tags={listing.tags}
+                            />
                         </div>
-                        <Tag 
-                            tags={listing.tags}
-                        />
+    
+                        <div className="listing--wrapper-right">
+                            <StarRating 
+                                rating={listing.rating}
+                            />
+                            <Host 
+                                host={listing.host}
+                            />
+                        </div>
                     </div>
-
-                    <div className="listing--wrapper-right">
-                        <StarRating 
-                            rating={listing.rating}
+                    <div className="collapsible-container">
+                        <Collapsible 
+                            type="paragraph"
+                            title="Description"
+                            description={listing.description}
+                            equipments={null}
                         />
-                        <Host 
-                            host={listing.host}
+    
+                        <Collapsible 
+                            type="list"
+                            title="Equipements"
+                            description={null}
+                            equipments={listing.equipments}
                         />
                     </div>
                 </div>
-                <div className="collapsible-container">
-                    <Collapsible 
-                        type="paragraph"
-                        title="Description"
-                        description={listing.description}
-                        equipments={null}
-                    />
+            </section>
+        )
+    }
 
-                    <Collapsible 
-                        type="list"
-                        title="Equipements"
-                        description={null}
-                        equipments={listing.equipments}
-                    />
-                </div>
-            </div>
-        </section>
-    )
+    
 }
